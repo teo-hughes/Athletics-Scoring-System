8547bcb4547f376fd6cc58250d6700ed2a70ac0b
Teo <hughes.t@etoncollege.org.uk>
2022-05-19 12:17:26 +0100

    Initial Commit

---
+++ Athletics Scoring System/Athletics_Scoring_SystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Athletics_Scoring_SystemApp.swift
+//  Athletics Scoring System
+//
+//  Created by Hughes, Teo (BJH) on 19/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Athletics_Scoring_SystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
